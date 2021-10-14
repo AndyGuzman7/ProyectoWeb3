ac29d72446a061de3f43b97eac00eed1bf0ae9cc
Andy Guzman <andyguzman117@gmail.com>
2021-10-14 15:41:59 -0400

    implementacion final del la ventana materia

---
+++ ProyectoWEBPAGE/js/functionality.js
@@ -0,0 +1,17 @@
+﻿if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
+}
+function sendDialogDelete(idCommentDelete, commentDeleteUpdate) {
+    document.getElementById("idCommentDelete").value = idCommentDelete;
+    document.getElementById("txtCommentDelete").value = commentDeleteUpdate;
+}
+
+
+function viewDataComent(idComment, matter, name, idUser, dateRegister, comment) {
+    document.getElementById("txtMatterUpdate").value = matter;
+    document.getElementById("txtIdUserUpdate").value = idUser;
+    document.getElementById("txtNameUpdate").value = name;
+    document.getElementById("txtCommentUpdate").value = comment;
+    document.getElementById("txtIdUpdate").value = idComment;
+    document.getElementById("txtDateRegister").value = dateRegister;
+}
