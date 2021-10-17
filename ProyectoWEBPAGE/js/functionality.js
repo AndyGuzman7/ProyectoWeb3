68690008cb62b5d8fd425b803a59a25995480187
Andy Guzman <andyguzman117@gmail.com>
2021-10-17 01:47:58 -0400

    implementacion del diseño de la ventana inicio

--- ProyectoWEBPAGE/js/functionality.js
+++ ProyectoWEBPAGE/js/functionality.js
@@ -15,3 +15,22 @@ function viewDataComent(idComment, matter, name, idUser, dateRegister, comment) 
     document.getElementById("txtIdUpdate").value = idComment;
     document.getElementById("txtDateRegister").value = dateRegister;
 }
+
+$('section.awSlider .carousel').carousel({
+    pause: "hover",
+    interval: 2000
+});
+
+var startImage = $('section.awSlider .item.active > img').attr('src');
+$('section.awSlider').append('<img src="' + startImage + '">');
+
+$('section.awSlider .carousel').on('slid.bs.carousel', function () {
+    var bscn = $(this).find('.item.active > img').attr('src');
+    $('section.awSlider > img').attr('src', bscn);
+});
+
+
+/*
+Philips ambilight tv
+Üzerine gleince duruyor slide
+*/
